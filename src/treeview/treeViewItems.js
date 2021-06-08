93fadc44c25493ffe2ca5ab4c1366ab461db5837
Ben Kuster <bkuster@virtualcitysystems.de>
2021-06-08 10:22:52 +0200

    added treeview item index

---
+++ src/treeview/treeViewItems.js
@@ -0,0 +1,4 @@
+import { VcsClassRegistry } from '@vcmap/core';
+import LayerTreeViewItem from '@/treeview/LayerTreeViewItem';
+
+VcsClassRegistry.registerClass('LayerTreeViewItem', LayerTreeViewItem);
