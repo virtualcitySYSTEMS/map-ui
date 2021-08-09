4a49f4f4ef881b33d75c05ab17157bc6b8f4aa2b
mchl18 <40480041+21e8@users.noreply.github.com>
2021-08-09 12:23:11 +0200

    Fix naming and behaviour

--- src/modules/window-manager/window.manager.js
+++ src/modules/window-manager/window.manager.js
@@ -13,14 +13,14 @@ import PositionParser from './util/position-parser';
  */
 
 /**
- * @typedef WindowState
- * @property {Object.<string, Window>} items
+ * @typedef WindowManagerState
+ * @property {Object.<string, WindowState>} items
  * @property {Object.<string, number>} zIndexMap
  * @property {number} zIndexMax
  */
 
 /**
- * @typedef Window
+ * @typedef WindowState
  * @property {boolean} visible
  * @property {string | number} id
  * @property {string | VueComponent} component
@@ -71,7 +71,7 @@ export class WindowManager {
   constructor() {
     this.onAdded = new VcsEvent();
     this.onRemoved = new VcsEvent();
-    /** @type {WindowState} */
+    /** @type {WindowManagerState} */
     this.state = reactive({
       items: {},
       zIndexMap: {},
@@ -81,7 +81,7 @@ export class WindowManager {
 
   /**
    * @param {string} id
-   * @returns {Window}
+   * @returns {WindowState}
    */
   get(id) {
     return this.state.items[id];
@@ -95,6 +95,10 @@ export class WindowManager {
     return !!this.get(id);
   }
 
+  /**
+   *
+   * @returns {Object.<string, WindowState>}
+   */
   getAll() {
     return Object.values(this.state.items);
   }
@@ -106,12 +110,15 @@ export class WindowManager {
    * Use this only to destroy a window, for hiding it call toggleViewVisible
    */
   remove(id) {
-    const windowComponent = this.state.items[id];
+    const windowComponent = this.get(id);
     if (!windowComponent) {
       throw new Error(`Cannot remove window with id '${id}' as it is not present.`);
     }
+
     this.onRemoved.raiseEvent(windowComponent);
     Vue.delete(this.state.items, id);
+    this.pullWindowsIn(windowComponent);
+
     this.getOrderedZIndex()
       .forEach((windowId, i) => {
         const zIndex = this.state.zIndexMax - i;
@@ -120,14 +127,14 @@ export class WindowManager {
 
     if (!windowComponent.isDocked) {
       nextTick(() => {
-        Object.keys(this.state.items).forEach(() => this.pullWindowsIn());
+        this.pullWindowsIn();
       });
     }
   }
 
 
   /**
-   * @param {Window} windowComponent
+   * @param {WindowState} windowComponent
    */
   toggle(windowComponent) {
     if (this.has(windowComponent.id)) {
@@ -146,35 +153,43 @@ export class WindowManager {
 
     const windowComponent = this.get(id);
 
-    const updatedWindow = {
+    Vue.set(this.state.items, windowComponent.id, {
       ...windowComponent,
       position: new PositionParser(position),
-    };
-
-    Vue.set(this.state.items, windowComponent.id, updatedWindow);
-  }
-
-  // Only pulls right to left right now
-  pullWindowsIn() {
-    const items = Object.values(this.state.items);
-    items.forEach((item) => {
-      const newRight = item.position.asNumber.right - item.width;
-      if (parseInt(item.position.right, 10) === 0) {
-        return;
-      }
-      const newPosition = new PositionParser({ ...item.position, right: `${newRight}px` });
-      if (items.some(i => i.position.isEqualTo(newPosition) && (i.id !== item.id))) {
-        return;
-      }
-      Vue.set(this.state.items, item.id, {
-        ...item,
-        position: newPosition,
-      });
     });
   }
 
   /**
-   * @param {Window} windowComponent
+   * @param {WindowState} removedWindow
+   */
+  pullWindowsIn(removedWindow) {
+    const dynamicWindowLeft = this.findWindowBySlot(WINDOW_SLOTS.dymanicLeft);
+    if (removedWindow.windowSlot === WINDOW_SLOTS.static && dynamicWindowLeft) {
+      Vue.set(this.state.items, dynamicWindowLeft.id, {
+        ...dynamicWindowLeft,
+        position: WINDOW_POSITIONS.topLeft,
+      });
+    }
+  }
+
+  /**
+   * @param {WINDOW_SLOT} windowSlot
+   * @returns {WindowState}
+   */
+  findWindowBySlot(windowSlot) {
+    return this.getAll().find(item => item.windowSlot === windowSlot);
+  }
+
+  /**
+   * @param {Position} position
+   * @returns {WindowState}
+   */
+  findWindowByPosition(position) {
+    return this.getAll().find(item => item.position.isEqualTo(position));
+  }
+
+  /**
+   * @param {WindowState} windowComponent
    */
   moveWindows(windowComponent) {
     switch (windowComponent.windowSlot) {
@@ -186,7 +201,7 @@ export class WindowManager {
         });
 
         windowComponent.position = WINDOW_POSITIONS.topLeft;
-        const windowAtSamePosition = this.getAll().find(item => item.position.isEqualTo(WINDOW_POSITIONS.topLeft));
+        const windowAtSamePosition = this.findWindowByPosition(WINDOW_POSITIONS.topLeft);
 
         if (windowAtSamePosition) {
           this.remove(windowAtSamePosition.id);
@@ -194,10 +209,10 @@ export class WindowManager {
         break;
       }
       case WINDOW_SLOTS.dymanicLeft: {
-        const staticWindow = this.getAll().find(item => item.windowSlot === WINDOW_SLOTS.static);
-        const dynamicWindow1 = this.getAll().find(item => item.windowSlot === WINDOW_SLOTS.dymanicLeft);
-        if (dynamicWindow1) {
-          this.remove(dynamicWindow1.id);
+        const staticWindow = this.findWindowBySlot(WINDOW_SLOTS.static);
+        const existing = this.findWindowBySlot(WINDOW_SLOTS.dymanicLeft);
+        if (existing) {
+          this.remove(existing.id);
         }
 
         if (staticWindow) {
@@ -209,7 +224,7 @@ export class WindowManager {
         break;
       }
       case WINDOW_SLOTS.dynamicRight: {
-        const existing = this.getAll().find(item => item.windowSlot === WINDOW_SLOTS.dynamicRight);
+        const existing = this.findWindowBySlot(WINDOW_SLOTS.dymanicRight);
 
         if (existing) {
           this.remove(existing.id);
@@ -224,7 +239,7 @@ export class WindowManager {
 
 
   /**
-   * @param {Window} windowComponent
+   * @param {WindowState} windowComponent
    */
   add(windowComponent) {
     if (!windowComponent.id) {
@@ -265,6 +280,9 @@ export class WindowManager {
       });
   }
 
+  /**
+   * @returns {Object.<string, number>}
+   */
   getOrderedZIndex() {
     return Object.keys(this.state.items)
       .sort((keyA, keyB) => this.state.zIndexMap[keyB] - this.state.zIndexMap[keyA]);
@@ -276,7 +294,7 @@ export class WindowManager {
   checkIfViewRegistered = (id) => {
     if (!this.has(id)) {
       throw new Error(
-        `Window with id '${id}' has not been registered!`,
+        `WindowState with id '${id}' has not been registered!`,
       );
     }
   };
