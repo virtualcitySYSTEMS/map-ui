a9b0fbcc1bc79c44c606830b280a19466f810090
Sandra Oppermann <soppermann@vc.systems>
2022-03-14 10:47:09 +0000

    fix badge border theming

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -20,21 +20,25 @@ const vuetify = new Vuetify({
     },
     themes: {
       light: {
+        basic: '#FFFFFF',
         primary: '#409D76',
         accent: '#EDEDED',
         warning: '#FFCE00',
         'gray-200': '#DEDEDE',
         gray: '#707070',
         // todo: change remaining colors
-        secondary: '#424242',
-        error: '#FF5252',
+        secondary: '#222222',
+        error: '#AA0000',
         info: '#2196F3',
         success: '#4CAF50',
       },
       dark: {
+        basic: '#000000',
         primary: '#409D76',
+        secondary: '#FFFFFF',
         accent: '#757575',
         warning: '#FFCE00',
+        error: '#FF5252',
       },
     },
   },
