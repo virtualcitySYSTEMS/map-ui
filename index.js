ef1e9e4605990d168d85804f0b24d55e37ce2de5
Jannes Bolling <jbolling@vc.systems>
2022-05-17 14:39:56 +0000

    change index.js to export all components

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 import './src/setup.js';
 import './src/styles/main.scss';
 
-export { WindowManager, WindowSlot, WindowPositions } from './src/manager/window/windowManager.js';
-export { default as ButtonManager, ButtonLocation } from './src/manager/buttonManager.js';
-export * from './src/actions/actionHelper.js';
-export { default as initApp } from './src/init.js';
-export { vuetify } from './src/vuePlugins/vuetify.js';
+export { default as StyleSelector } from './src/actions/styleSelector.vue';
+export {
+  createMapButtonAction,
+  createToggleAction,
+  createOverviewMapAction,
+  createModalAction,
+  createLinkAction,
+  createGoToViewpointAction,
+} from './src/actions/actionHelper.js';
+export {
+  createStateRefAction,
+} from './src/actions/stateRefAction.js';
+
 export { default as EmptyCmpt } from './src/application/empty-cmp.vue';
+export { default as Navbar } from './src/application/Navbar.vue';
+export { default as VcsApp } from './src/application/VcsApp.vue';
+export { default as VcsMap } from './src/application/VcsMap.vue';
+
+export {
+  default as ContentTreeCollection,
+  createContentTreeCollection,
+} from './src/contentTree/contentTreeCollection.js';
 export { default as ContentTreeItem } from './src/contentTree/contentTreeItem.js';
 export { default as GroupContentTreeItem } from './src/contentTree/groupContentTreeItem.js';
-export { default as LayerContentTreeItem } from './src/contentTree/layerContentTreeItem.js';
+export {
+  default as LayerContentTreeItem,
+  setViewpointAction,
+  getStateFromLayer,
+} from './src/contentTree/layerContentTreeItem.js';
 export { default as LayerGroupContentTreeItem } from './src/contentTree/layerGroupContentTreeItem.js';
+export { default as LayerTree } from './src/contentTree/LayerTree.vue';
 export { default as NodeContentTreeItem } from './src/contentTree/nodeContentTreeItem.js';
 export { default as ObliqueCollectionContentTreeItem } from './src/contentTree/obliqueCollectionContentTreeItem.js';
 export { default as SubContentTreeItem } from './src/contentTree/subContentTreeItem.js';
 export { default as VcsObjectContentTreeItem } from './src/contentTree/vcsObjectContentTreeItem.js';
 export { default as ViewPointContentTreeItem } from './src/contentTree/viewPointContentTreeItem.js';
+
+export { default as WindowComponent } from './src/manager/window/WindowComponent.vue';
+export { default as WindowComponentHeader } from './src/manager/window/WindowComponentHeader.vue';
+export { default as VcsWindowManager } from './src/manager/window/WindowManager.vue';
+export { WindowManager, WindowSlot, WindowPositions } from './src/manager/window/windowManager.js';
+
+export { default as ButtonManager, ButtonLocation, getActionsByLocation } from './src/manager/buttonManager.js';
+
+export { default as MapNavCompass } from './src/navigation/mapNavCompass.vue';
+export { default as MapNavigation } from './src/navigation/mapNavigation.vue';
+export { default as ObliqueRotation } from './src/navigation/obliqueRotation.vue';
+export { default as OrientationToolsButton } from './src/navigation/orientationToolsButton.vue';
+export { getWindowComponentOptions, default as OverviewMap } from './src/navigation/overviewMap.js';
+export { default as OverviewMapClickedInteraction } from './src/navigation/overviewMapClickedInteraction.js';
+export { default as TiltSlider } from './src/navigation/tiltSlider.vue';
+export { default as VcsCompass } from './src/navigation/vcsCompass.vue';
+export { default as VcsZoomButton } from './src/navigation/vcsZoomButton.vue';
+
+export { i18n } from './src/vuePlugins/i18n.js';
+export { vuetify } from './src/vuePlugins/vuetify.js';
+export { default as initApp } from './src/init.js';
+export {
+  vcsAppSymbol,
+  pluginFactorySymbol,
+  isValidPackageName,
+  loadPlugin,
+  serializePlugin,
+  deserializePlugin,
+} from './src/pluginHelper.js';
+export { default as VcsUiApp } from './src/vcsUiApp.js';
+
