eb8b0031f50681bf97247423f982e02cfcebedfb
Sandra Oppermann <soppermann@vc.systems>
2023-01-31 09:56:38 +0000

    Fix item alignments for vcsList: Title to  the left

--- src/components/lists/VcsList.vue
+++ src/components/lists/VcsList.vue
@@ -410,8 +410,6 @@
     .v-list{
       .v-list-item {
         padding: 4px 8px 4px 16px;
-        display: grid;
-        grid-template-columns: auto auto;
         &:after{
           display: none;
         }
