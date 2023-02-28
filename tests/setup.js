9cbc988413f4932c0516af7caae1745df067174c
Tobias Krauth <tkrauth@vc.systems>
2023-02-28 15:12:43 +0000

    Update Core to version with Openlayers 7.2

--- tests/setup.js
+++ tests/setup.js
@@ -2,11 +2,13 @@
 import { vi } from 'vitest';
 import CanvasRenderingContext2D from 'jest-canvas-mock/lib/classes/CanvasRenderingContext2D.js';
 import Path2D from 'jest-canvas-mock/lib/classes/Path2D.js';
+import ResizeObserver from 'resize-observer-polyfill';
 
 
 global.jest = vi;
 global.CanvasRenderingContext2D = CanvasRenderingContext2D;
 global.Path2D = Path2D;
+global.ResizeObserver = ResizeObserver;
 
 import 'jest-canvas-mock';
 import '../src/setup.js';
