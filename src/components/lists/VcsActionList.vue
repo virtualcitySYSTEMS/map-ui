53d16eb78737e2261d10f1ef65850ab2614ffb36
Jannes Bolling <jbolling@vc.systems>
2024-07-12 10:05:40 +0000

    update ActionList

--- src/components/lists/VcsActionList.vue
+++ src/components/lists/VcsActionList.vue
@@ -1,40 +1,48 @@
 <template>
-  <v-list v-if="actions.length > 0" rounded>
-    <VcsTooltip
+  <v-list v-if="actions.length > 0" rounded class="vcs-action-list">
+    <v-list-item
       v-for="(action, index) in actions"
       :key="`${action.name}-${index}`"
-      :tooltip="action.title"
-      :tooltip-position="tooltipPosition"
-      v-bind="{ ...tooltipProps }"
+      :class="action.active ? 'text-primary' : ''"
+      :disabled="action.disabled || disabled"
+      @click="action.callback($event)"
+      v-bind="{ ...$attrs }"
     >
-      <template #activator="{ props }">
-        <v-list-item
-          :class="action.active ? 'text-primary' : ''"
-          :disabled="action.disabled || disabled"
-          @click="action.callback($event)"
-          v-bind="{ ...$attrs, ...props }"
-        >
-          <template #prepend v-if="showIcon && action.icon">
-            <v-icon>{{ action.icon }}</v-icon>
-          </template>
-          <v-list-item-title>{{ $st(action.name) }}</v-list-item-title>
-        </v-list-item>
+      <template #prepend v-if="showIcon && action.icon">
+        <v-icon :size="iconSize">{{ action.icon }}</v-icon>
       </template>
-    </VcsTooltip>
+      <v-list-item-title>{{ $st(action.name) }}</v-list-item-title>
+      <v-tooltip
+        v-if="action.title"
+        activator="parent"
+        :text="$st(action.title)"
+        :location="tooltipPosition"
+        v-bind="{ ...tooltipProps }"
+      />
+    </v-list-item>
   </v-list>
 </template>
 <style lang="scss" scoped>
-  :deep(.v-list-item-title:hover) {
-    color: rgb(var(--v-theme-primary));
+  :deep(.v-list-item:hover) {
+    .v-list-item-title {
+      color: rgb(var(--v-theme-primary));
+    }
   }
   :deep(.v-list-item .v-list-item__prepend .v-list-item__spacer) {
     width: 8px;
   }
 </style>
 <script>
+  import { computed } from 'vue';
   import { is } from '@vcsuite/check';
-  import { VIcon, VList, VListItem, VListItemTitle } from 'vuetify/components';
-  import VcsTooltip from '../notification/VcsTooltip.vue';
+  import {
+    VIcon,
+    VList,
+    VListItem,
+    VListItemTitle,
+    VTooltip,
+  } from 'vuetify/components';
+  import { useFontSize } from '../../vuePlugins/vuetify.js';
 
   /**
    * pattern to check actions
@@ -69,22 +77,21 @@
 
   /**
    * @description
-   * A component rendering a list of actions with overflow mechanic using
-   * {@link https://vuetifyjs.com/en/api/v-list-item-group/|vuetify v-list-item-group} and {@link VcsTooltip}.
+   * A component rendering a list of actions
    * @vue-prop {Array<VcsAction>} actions - Array of actions
-   * @vue-prop {('bottom' | 'left' | 'top' | 'right')}  tooltipPosition - Position of the tooltip.
-   * @vue-prop {Object<string, any>}                    tooltipProps - Properties to be passed to VcsTooltip {@link https://vuetifyjs.com/en/api/v-tooltip/#props|vuetify v-tooltip}
+   * @vue-prop {('bottom' | 'left' | 'top' | 'right')}  tooltipPosition - Position of the tooltip. (action.title will be rendered as a tooltip)
+   * @vue-prop {Object<string, any>}                    tooltipProps - Properties to be passed to v-tooltip
    * @vue-prop {boolean}                                [showIcon=false] - Whether list item icons should be displayed.
    * @vue-prop {boolean}                                [disabled=false] - disable all actions
    */
   export default {
     name: 'VcsActionList',
     components: {
-      VcsTooltip,
       VList,
       VListItem,
       VIcon,
       VListItemTitle,
+      VTooltip,
     },
     props: {
       actions: {
@@ -108,6 +115,15 @@
         type: Boolean,
         default: false,
       },
+    },
+    setup() {
+      const fontSize = useFontSize();
+      const iconSize = computed(() => {
+        return fontSize.value * (1.2 + 0.1 / 3);
+      });
+      return {
+        iconSize,
+      };
     },
   };
 </script>
